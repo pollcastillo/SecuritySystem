bf1e69867a630cfc59a1c2126f368f50a84cea23
Poll Castillo <alejandrocordovafree@icloud.com>
2025-05-17 06:55:38 -0500

    update some scripts

---
+++ source/typescript/_core/api/getData.ts
@@ -0,0 +1,7 @@
+export default async function getData<T>(url: string): Promise<T> {
+    const data: T = await fetch(`../../data/${url}.json`)
+        .then(response => response.json());
+    return await data;
+}
+
+export type dataURI = "../../data/Note.json" | "../../data/User.json" | "../../data/Marcation.json" | "../../data/Department.json" | "../../data/Notification.json" | "../../data/Visit.json";
